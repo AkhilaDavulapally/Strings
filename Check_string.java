1f33e28afb1946ba56c845e9853ed3e9a28266b4
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-04-21 20:18:21 +0530

    Create Check_string.java

---
+++ Check_string.java
@@ -0,0 +1,9 @@
+package com.string;
+public class Check_string {
+	public static void main(String[] args) {
+		String s="Hello";
+		String s1=new String("Hello");
+		System.out.println(s==s1);
+		System.out.println(s.equals(s1));
+	}
+}
