74cff58ef3268548fe9f3087868a86ca3ef9b2a8
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-04-24 21:15:51 +0530

    Create String_Builder.java

---
+++ String_Builder.java
@@ -0,0 +1,7 @@
+package com.string;
+public class String_Builder {
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("Tilak");
+		System.out.println(s.append('s'));
+	}
+}
