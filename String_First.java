1ff21176fe6df3b8f30b3eeab6145c76c8a14de5
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-04-25 18:21:05 +0530

    Create String_First.java

---
+++ String_First.java
@@ -0,0 +1,14 @@
+package com.string;
+public class String_First {
+	public static void main(String[] args) {
+		String s="saiRam";
+		System.out.println(s);
+		System.out.println(s.toString());
+		String_First s1=new String_First();
+		System.out.println(s1);
+	}
+	public String toString()
+	{
+		return "Hello";
+	}
+}
