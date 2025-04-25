aa1d3b556d46686158f20055eeb3faac609e5a80
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-04-25 18:22:19 +0530

    Create String_Length.java

---
+++ String_Length.java
@@ -0,0 +1,11 @@
+package com.string;
+import java.util.Scanner;
+public class String_Length {
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the string");
+			String s=sc.next();
+			int l=s.length();
+			System.out.println(l);
+		}
+	}
